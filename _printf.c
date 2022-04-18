6769004fde5501435f158e06128b16a10ada29bb
lewisokello <91951659+lewisokello@users.noreply.github.com>
2022-04-18 07:21:48 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -9,26 +9,25 @@
  */
 static int (*check_for_specifiers(const char *format))(va_list)
 {
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
+unsigned int i;
+print_t p[] = {
+{"c", print_c},
+{"s", print_s},
+{"i", print_i},
+{"d", print_d},
+{"u", print_u},
+{"b", print_b},
+{"o", print_o},
+{"x", print_x},
+{"X", print_X},
+{"p", print_p},
+{"S", print_S},
+{"r", print_r},
+{"R", print_R},
+{NULL, NULL}
+};
 	{
+		for (i = 0; p[i].t != NULL; i++)
 		if (*(p[i].t) == *format)
 		{
 			break;
